447066322cde1c9105febe6de6b535afec69451b
AshikaVerma <ashikaverma5736@gmail.com>
2025-06-28 10:19:30 +0530

    ...

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://task-manager-fawn-mu.vercel.app",
+    origin: "https:task-manager-fawn-mu.vercel.app",
+    
     credentials: true,
   })
 );
