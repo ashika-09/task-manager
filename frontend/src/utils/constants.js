d2ead29e569ac0e9a229d7890cacd71d0c96966e
AshikaVerma <ashikaverma5736@gmail.com>
2025-06-28 09:56:25 +0530

    add url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000";
+//export const BASE_URL = "http://localhost:5000";
 
-// export const BASE_URL = "https://task-manager-1buf.onrender.com";
+ export const BASE_URL = "https://task-manager-ehmj.onrender.com";
